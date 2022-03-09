bb5308c9459ea84a162885df77abb7acadc0a59f
Wilton Souza <willsouzafilho@gmail.com>
2022-03-09 14:21:35 -0300

    Finishing the game implementation

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,13 +1,25 @@
 defmodule ExMon.Game.Actions do
   alias ExMon.Game
+  alias ExMon.Game.Actions.{Attack, Heal}
+
+  def attack(move) do
+    case Game.turn() do
+      :player -> Attack.attack_opponent(:computer, move)
+      :computer -> Attack.attack_opponent(:player, move)
+    end
+  end
+
+  def heal do
+    case Game.turn() do
+      :player -> Heal.heal_life(:player)
+      :computer -> Heal.heal_life(:computer)
+    end
+  end
 
   def fetch_move(move) do
     Game.player()
     |> Map.get(:moves)
     |> find_move(move)
-  end
-
-  def attack(move) do
   end
 
   defp find_move(moves, move) do
